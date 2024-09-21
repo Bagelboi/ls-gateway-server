960da6934957f7cd92588448bf07e08a7fff96be
Bagelboi <donkeysoda22@gmail.com>
2024-09-21 02:03:15 -0300

    first commit

---
+++ src/main/java/org/daniel/LS_gateway_service/LsGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package org.daniel.LS_gateway_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LsGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LsGatewayServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/daniel/LS_gateway_service/LsGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.daniel.LS_gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LsGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
